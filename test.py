47e75e3a3f307bb182d3dfb164509069cdf812f3
MIzarNPC <guseynovroma93@gmail.com>
2022-01-23 18:32:19 +0300

    lesson

---
+++ test.py
@@ -0,0 +1,14 @@
+print("Hello world")
+for i in range(5):
+  n = i*4
+  print(n)
+
+print("Hello world")
+for i in range(5):
+    n = i*4
+    print(n)
+
+print("Hello world")
+for i in range(5):
+      n = i*4
+      print(n)
